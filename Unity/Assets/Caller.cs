af8c02e49d3e7f91926f71f90c2adc8ccdeef8c9
Meeka0194 <145824503+Meeka0194@users.noreply.github.com>
2026-02-15 20:45:11 -0700

    Unity

---
+++ Unity/Assets/Caller.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Caller : MonoBehaviour
+{
+    [SerializeField] private Receiver receiver;
+
+    private void Start()
+    {
+        Debug.Log("Hello Friend");
+
+        receiver.OnCalled();
+    }
+}
