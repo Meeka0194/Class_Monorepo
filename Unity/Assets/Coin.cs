d974539b511b571ddcccffae24502ae197dd7506
Meeka0194 <145824503+Meeka0194@users.noreply.github.com>
2026-02-27 18:53:01 -0700

    Unity

---
+++ Unity/Assets/Coin.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.tag == "Player")
+        {
+            Debug.LogError("My Brain is Bad");
+            Debug.Log("Coin Collected");
+        }
+
+    }
+}
